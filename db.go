071fa842e823b21689ea4d0adbb5c75e84dd2fb3
cramt <mads9220@gmail.com>
2019-10-15 11:21:26 +0200

    fixed database bug, hopefully

--- db.go
+++ db.go
@@ -55,7 +55,7 @@ func openDBConnection() {
 
 func startupAddAllUsers(users []string) {
 	query := "INSERT INTO users (discord_id) VALUES "
-	values := []interface{}{}
+	var values []interface{}
 	for i, s := range users {
 		values = append(values, s)
 		numFields := 1
@@ -75,7 +75,7 @@ func startupAddAllUsers(users []string) {
 
 func startupAddAllGuilds(guilds []string) {
 	query := "INSERT INTO prefixes (guild_id, prefix) VALUES "
-	values := []interface{}{}
+	var values []interface{}
 	for i, s := range guilds {
 		values = append(values, s, "?")
 		numFields := 2
@@ -104,26 +104,26 @@ func setPrefixForGuild(guildId string, prefix string) {
 func getGuildPrefix(guildId string) *string {
 	rows, err := DB.Query("SELECT prefix FROM prefixes WHERE guild_id = $1", guildId)
 	logErr(err)
+	defer rows.Close()
 	for rows.Next() {
 		var prefix string
 		err = rows.Scan(&prefix)
 		logErr(err)
 		return &prefix
 	}
-	defer rows.Close()
 	return nil
 }
 
 func getUserFromNickname(nickname string) sql.NullString {
 	rows, err := DB.Query("SELECT discord_id FROM users WHERE nick_name = $1", nickname)
 	logErr(err)
+	defer rows.Close()
 	for rows.Next() {
 		var discordId sql.NullString
 		err = rows.Scan(&discordId)
 		logErr(err)
 		return discordId
 	}
-	defer rows.Close()
 	return sql.NullString{
 		String: "",
 		Valid:  false,
@@ -141,6 +141,7 @@ func setUsersNickname(user *User) {
 func getUser(discordId string) *User {
 	rows, err := DB.Query("SELECT nick_name FROM users WHERE discord_id = $1", discordId)
 	logErr(err)
+	defer rows.Close()
 	for rows.Next() {
 		var nickname sql.NullString
 		err = rows.Scan(&nickname)
@@ -150,7 +151,6 @@ func getUser(discordId string) *User {
 			discordId: discordId,
 		}
 	}
-	defer rows.Close()
 	return nil
 }
 
@@ -175,6 +175,7 @@ func getUsersPointsReceived(discordId string) ([]*Points, []sql.NullString) {
 	logErr(err)
 	var points []*Points
 	var nicknames []sql.NullString
+	defer rows.Close()
 	for rows.Next() {
 		var giverId string
 		var nickname sql.NullString
@@ -188,19 +189,18 @@ func getUsersPointsReceived(discordId string) ([]*Points, []sql.NullString) {
 			amount:   amount,
 		})
 	}
-	defer rows.Close()
 	return points, nicknames
 }
 
 func getUsersPointsReceivedFromOtherUser(receiverId string, giverId string) sql.NullInt64 {
 	rows, err := DB.Query("SELECT points.amount FROM points WHERE points.id = $1", receiverId+"_"+giverId)
 	logErr(err)
+	defer rows.Close()
 	for rows.Next() {
 		var amount sql.NullInt64
 		err = rows.Scan(&amount)
 		return amount
 	}
-	defer rows.Close()
 	return sql.NullInt64{
 		Int64: 0,
 		Valid: false,
@@ -210,6 +210,7 @@ func getUsersPointsReceivedFromOtherUser(receiverId string, giverId string) sql.
 func getUsersPointsGiven(discordId string) ([]*Points, []sql.NullString) {
 	rows, err := DB.Query("SELECT points.receiver_id, users.nick_name, points.amount FROM points INNER JOIN users ON users.discord_id = points.receiver_id WHERE points.giver_id = $1 ORDER BY points.amount DESC", discordId)
 	logErr(err)
+	defer rows.Close()
 	var points []*Points
 	var nicknames []sql.NullString
 	for rows.Next() {
@@ -225,6 +226,5 @@ func getUsersPointsGiven(discordId string) ([]*Points, []sql.NullString) {
 			amount:   amount,
 		})
 	}
-	defer rows.Close()
 	return points, nicknames
 }
